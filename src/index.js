ea00e0fe955056329d7a9ac22792823f939346e4
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-12-12 20:52:25 +0100

    index,:js: css: about

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-async function getData()  {
-    let resp = await fetch("http://localhost:8080/icons/pijl");
+async function getData(icon)  {
+    let resp = await fetch(`http://localhost:8080/icons/${icon}`);
     return await resp.text();
 }
 
 
 async function showPijl()  {
-    let pijl = await getData();
+    let pijl = await getData("pijl");
     document.getElementById("pijl").insertAdjacentHTML("beforeend", pijl);
 }
 
+async function showMoney()  {
+    let money = await getData("money");
+    document.getElementById("icons").insertAdjacentHTML("beforeend", money);
+}
+
+async function showBuilding()  {
+    let building = await getData("building");
+    document.getElementById("icons").insertAdjacentHTML("beforeend", building);
+}
+
+async function showTravel()  {
+    let travel = await getData("travel");
+    document.getElementById("icons").insertAdjacentHTML("beforeend", travel);
+}
+
 showPijl();
+showMoney();
+showBuilding();
+showTravel();
