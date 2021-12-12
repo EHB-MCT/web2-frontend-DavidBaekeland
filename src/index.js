62dfae46fd51d083e9e26ee66106ed6b726e02ac
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-12-12 18:57:14 +0100

    index: js: css: start; logo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('test');
+async function getData()  {
+    let resp = await fetch("http://localhost:8080/icons/pijl");
+    return await resp.text();
+}
+
+
+async function showPijl()  {
+    let pijl = await getData();
+    document.getElementById("test").insertAdjacentHTML("beforeend", pijl);
+}
+
+showPijl();
