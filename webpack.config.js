bf1b79d9a90bdda8e9d5b178493afefa911ca98c
DavidBaekeland <71763779+DavidBaekeland@users.noreply.github.com>
2021-12-14 11:07:06 +0100

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+// npx webpack: normaal automatich -> ook aanpassen
+
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  watch: true
+};
